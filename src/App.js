1b87b5ed5d3edd44af8e2122db96c1aaf21400db
Mayank Choudhary <mayankchd10@gmail.com>
2024-03-15 23:44:12 +0530

    feat: custom font familiy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className='bold border'>
+        <p className='bold border font-raleway'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
