08cc2b56a06149bb5d5df41d1a9dcc634ed36ad1
Mayank Choudhary <mayankchd10@gmail.com>
2024-03-21 13:37:09 +0530

    feat: Mouse following button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
+import { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className='
-    bg-gray-100
-    h-screen
-    grid
-    place-items-center'>
-      <div className='grid grid-cols-3 gap-4'>
-        <div>🔥</div>
-        <div>🔥</div>
-        <div>🔥</div>
-        <div>🔥</div>
-        <div>🔥</div>
-        <div>🔥</div>
-        <div>🔥</div>
-        <div>🔥</div>
-        <div>🔥</div>
-      </div>
-    </div>
-  );
+  const [gradientStyle, setGradientStyle] 
+  = useState({
+    background: 
+    'linear-gradient(to right, #ff0000, #0000ff)',
+  });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const mouseX = e.pageX;
+      const windowWidth = window.innerWidth;
+      const percentageX = (mouseX / windowWidth) * 100;
+      const gradient = 
+      `linear-gradient(to right,
+      #ff0000 ${percentageX}%,
+      #0000ff)`;
+      setGradientStyle({ background: gradient });
+    };
+    window.addEventListener('mousemove',
+    handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove',
+      handleMouseMove);
+    };
+  }, []);
+
+  return <div className="
+  bg-black 
+  h-screen 
+  flex 
+  items-center 
+  justify-center">
+    <button
+    style={gradientStyle}
+    className='bg-transparent
+    hover:bg-transparent
+    text-white 
+    font-bold 
+    py-2 px-4 
+    rounded-full 
+    shadow-md 
+    transition 
+    duration-300 
+    ease-in-out'>Mouse Following Button</button>
+  </div>
+
 }
 
 export default App;
