4d8dc0d51057aafd81b1c640959013694b0400a2
Mayank Choudhary <mayankchd10@gmail.com>
2024-03-24 14:02:28 +0530

    responsive layout - utility classes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,28 @@
-import { useState } from 'react';
 import './App.css';
-import { useEffect } from 'react';
 
 function App() {
-  const [gradientStyle, setGradientStyle] 
-  = useState({
-    background: 
-    'linear-gradient(to right, #ff0000, #0000ff)',
-  });
+  return (
+    <div className="bg-gray-900
+    p-8
+    min-h-screen 
+    grid 
+    place-items-center
+    ">
+      <div className='
+      max-w-screen-lg
+      mx-auto
+      grid gap-6
+      sm:grid-cols-2 
+      md:grid-cols-3 
+      lg:grid-cols-4 
+      xl:grid-cols-5'>
 
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      const mouseX = e.pageX;
-      const windowWidth = window.innerWidth;
-      const percentageX = (mouseX / windowWidth) * 100;
-      const gradient = 
-      `linear-gradient(to right,
-      #ff0000 ${percentageX}%,
-      #0000ff)`;
-      setGradientStyle({ background: gradient });
-    };
-    window.addEventListener('mousemove',
-    handleMouseMove);
-
-    return () => {
-      window.removeEventListener('mousemove',
-      handleMouseMove);
-    };
-  }, []);
-
-  return <div className="
-  bg-black 
-  h-screen 
-  flex 
-  items-center 
-  justify-center">
-    <button
-    style={gradientStyle}
-    className='bg-transparent
-    hover:bg-transparent
-    text-white 
-    font-bold 
-    py-2 px-4 
-    rounded-full 
-    shadow-md 
-    transition 
-    duration-300 
-    ease-in-out'>Mouse Following Button</button>
-  </div>
-
+      </div>
+    </div>
+  );
 }
 
+
 export default App;
+
+
